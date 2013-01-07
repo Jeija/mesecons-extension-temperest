f50933e73777eafa522cfd48c705490c211617a0
Anthony Zhang <azhang9@gmail.com>
2013-01-07 17:08:51 -0500

    Update mod to work with mesecons nodedef and nextgen changes.

--- mesecons_temperest/init.lua
+++ mesecons_temperest/init.lua
@@ -1,112 +1,56 @@
 --TEMPEREST-PLUG
 
-local set_node_on
-local set_node_off
-
-if true then
-	set_node_on = function(pos)
-		local node = minetest.env:get_node(pos)
-		if node.name=="mesecons_temperest:mesecon_socket_off" then
-			minetest.env:add_node(pos, {name="mesecons_temperest:mesecon_socket_on"})
-			nodeupdate(pos)
-			mesecon:receptor_on(pos)
-		elseif node.name=="mesecons_temperest:mesecon_inverter_on" then
-			minetest.env:add_node(pos, {name="mesecons_temperest:mesecon_inverter_off"})
-			nodeupdate(pos)
-			mesecon:receptor_off(pos)
-		end
-	end
-	
-	set_node_off = function(pos)
-		node = minetest.env:get_node(pos)
-		if node.name=="mesecons_temperest:mesecon_socket_on" then
-			minetest.env:add_node(pos, {name="mesecons_temperest:mesecon_socket_off"})
-			nodeupdate(pos)
-			mesecon:receptor_off(pos)
-		elseif node.name=="mesecons_temperest:mesecon_inverter_off" then
-			minetest.env:add_node(pos, {name="mesecons_temperest:mesecon_inverter_on"})
-			nodeupdate(pos)
-			mesecon:receptor_on(pos)
-		end
-	end
-else
-	set_node_on = function(pos)
-		local node = minetest.env:get_node(pos)
-		if node.name=="mesecons_temperest:mesecon_socket_off" then
-			minetest.env:add_node(pos, {name="mesecons_temperest:mesecon_socket_on"})
-			nodeupdate(pos)
-			mesecon:receptor_on(pos)
-		end
-	end
-	
-	set_node_off = function(pos)
-		node = minetest.env:get_node(pos)
-		if node.name=="mesecons_temperest:mesecon_socket_on" then
-			minetest.env:add_node(pos, {name="mesecons_temperest:mesecon_socket_off"})
-			nodeupdate(pos)
-			mesecon:receptor_off(pos)
-		end
-	end
-end
+local plug_rules = {
+	{x=-1, y=0, z=0},
+	{x=1, y=0, z=0},
+	{x=0, y=-1, z=0},
+	{x=0, y=1, z=0},
+	{x=0, y=0, z=-1},
+	{x=0, y=0, z=1},
+}
 
 local plug_on = function(pos)
-	local node = minetest.env:get_node(pos)
-	if node.name=="mesecons_temperest:mesecon_plug" then
-		local lnode = minetest.env:get_node({x=pos.x-1, y=pos.y, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_on({x=pos.x-2, y=pos.y, z=pos.z}) end
-		
-		local lnode = minetest.env:get_node({x=pos.x+1, y=pos.y, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_on({x=pos.x+2, y=pos.y, z=pos.z}) end
-		
-		lnode = minetest.env:get_node({x=pos.x, y=pos.y-1, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_on({x=pos.x, y=pos.y-2, z=pos.z}) end
-		
-		lnode = minetest.env:get_node({x=pos.x, y=pos.y+1, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_on({x=pos.x, y=pos.y+2, z=pos.z}) end
-		
-		local lnode = minetest.env:get_node({x=pos.x, y=pos.y, z=pos.z-1}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_on({x=pos.x, y=pos.y, z=pos.z-2}) end
-		
-		local lnode = minetest.env:get_node({x=pos.x, y=pos.y, z=pos.z+1}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_on({x=pos.x, y=pos.y, z=pos.z+2}) end
+	for i, rule in ipairs(plug_rules) do
+		local checkpos = {x=pos.x + rule.x, y=pos.y + rule.y, z=pos.z + rule.z}
+		if minetest.env:get_node(checkpos).name == "air" then
+			checkpos.x, checkpos.y, checkpos.z = checkpos.x + rule.x, checkpos.y + rule.y, checkpos.z + rule.z
+			local node = minetest.env:get_node(checkpos)
+			if node.name == "mesecons_temperest:mesecon_socket_off" then
+				mesecon:swap_node(checkpos, "mesecons_temperest:mesecon_socket_on")
+				mesecon:receptor_on(checkpos)
+			elseif node.name == "mesecons_temperest:mesecon_inverter_on" then
+				mesecon:swap_node(checkpos, "mesecons_temperest:mesecon_inverter_off")
+				mesecon:receptor_off(checkpos)
+			end
+		end
 	end
 end
 
 local plug_off = function(pos)
-	local node = minetest.env:get_node(pos)
-	if node.name=="mesecons_temperest:mesecon_plug" then
-		lnode = minetest.env:get_node({x=pos.x-1, y=pos.y, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_off({x=pos.x-2, y=pos.y, z=pos.z}) end
-		
-		lnode = minetest.env:get_node({x=pos.x+1, y=pos.y, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_off({x=pos.x+2, y=pos.y, z=pos.z}) end
-		
-		lnode = minetest.env:get_node({x=pos.x, y=pos.y-1, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_off({x=pos.x, y=pos.y-2, z=pos.z}) end
-		
-		lnode = minetest.env:get_node({x=pos.x, y=pos.y+1, z=pos.z}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_off({x=pos.x, y=pos.y+2, z=pos.z}) end
-		
-		lnode = minetest.env:get_node({x=pos.x, y=pos.y, z=pos.z-1}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_off({x=pos.x, y=pos.y, z=pos.z-2}) end
-		
-		lnode = minetest.env:get_node({x=pos.x, y=pos.y, z=pos.z+1}) --a node between this node and the one two nodes away
-		if lnode.name=="air" then set_node_off({x=pos.x, y=pos.y, z=pos.z+2}) end
+	for i, rule in ipairs(plug_rules) do
+		local checkpos = {x=pos.x + rule.x, y=pos.y + rule.y, z=pos.z + rule.z}
+		if minetest.env:get_node(checkpos).name == "air" then
+			checkpos.x, checkpos.y, checkpos.z = checkpos.x + rule.x, checkpos.y + rule.y, checkpos.z + rule.z
+			local node = minetest.env:get_node(checkpos)
+			if node.name == "mesecons_temperest:mesecon_socket_on" then
+				mesecon:swap_node(checkpos, "mesecons_temperest:mesecon_socket_off")
+				mesecon:receptor_off(checkpos)
+			elseif node.name == "mesecons_temperest:mesecon_inverter_off" then
+				mesecon:swap_node(checkpos, "mesecons_temperest:mesecon_inverter_on")
+				mesecon:receptor_on(checkpos)
+			end
+		end
 	end
 end
 
-mesecon:register_on_signal_on(plug_on)
-mesecon:register_on_signal_off(plug_off)
-mesecon:register_effector("mesecons_temperest:mesecon_plug", "mesecons_temperest:mesecon_plug")
-
 minetest.register_node("mesecons_temperest:mesecon_plug", {
+	description = "Plug",
 	drawtype = "nodebox",
 	paramtype = "light",
-	is_ground_content = true,
 	tile_images = {"jeija_mesecon_plug.png"},
 	inventory_image = "jeija_mesecon_plug.png",
 	wield_image = "jeija_mesecon_plug.png",
-	groups = {dig_immediate=2, mesecon_effector_on = 1, mesecon_effector_off = 1, mesecon = 2},
+	groups = {dig_immediate=2, mesecon_effector_on=1, mesecon_effector_off=1, mesecon=2},
 	walkable = false,
 	selection_box = {
 		type = "fixed",
@@ -116,17 +60,20 @@ minetest.register_node("mesecons_temperest:mesecon_plug", {
 		type = "fixed",
 		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
 	},
-	description = "Plug",
 	after_place_node = plug_off,
-	after_dig_node = plug_off
+	after_dig_node = plug_off,
+	mesecons = {effector = {
+		action_on = plug_on,
+		action_off = plug_off,
+	}},
 })
 
 minetest.register_craft({
-	output = '"mesecons_temperest:mesecon_plug" 2',
+	output = 'mesecons_temperest:mesecon_plug 2',
 	recipe = {
-		{'', '"group:mesecon_conductor_craftable"', ''},
-		{'"group:mesecon_conductor_craftable"', '"default:steel_ingot"', '"group:mesecon_conductor_craftable"'},
-		{'', '"group:mesecon_conductor_craftable"', ''},
+		{'', 'group:mesecon_conductor_craftable', ''},
+		{'group:mesecon_conductor_craftable', 'default:steel_ingot', 'group:mesecon_conductor_craftable'},
+		{'', 'group:mesecon_conductor_craftable', ''},
 	}
 })
 
@@ -136,11 +83,10 @@ minetest.register_node("mesecons_temperest:mesecon_socket_off", {
 	description = "Socket",
 	drawtype = "nodebox",
 	paramtype = "light",
-	is_ground_content = true,
 	tile_images = {"jeija_mesecon_socket_off.png"},
 	inventory_image = "jeija_mesecon_socket_off.png",
 	wield_image = "jeija_mesecon_socket_off.png",
-	groups = {dig_immediate=2, mesecon_effector_on = 1, mesecon_effector_off = 1, mesecon = 2},
+	groups = {dig_immediate=2, mesecon_effector_on=1, mesecon_effector_off=1, mesecon=2},
 	walkable = false,
 	selection_box = {
 		type = "fixed",
@@ -150,14 +96,16 @@ minetest.register_node("mesecons_temperest:mesecon_socket_off", {
 		type = "fixed",
 		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
 	},
+	mesecons = {receptor = {
+		state = mesecon.state.off
+	}},
 })
 
 minetest.register_node("mesecons_temperest:mesecon_socket_on", {
 	drawtype = "nodebox",
 	paramtype = "light",
-	is_ground_content = true,
 	tile_images = {"jeija_mesecon_socket_on.png"},
-	groups = {dig_immediate=2,not_in_creative_inventory=1, mesecon_effector_on = 1, mesecon_effector_off = 1, mesecon = 2},
+	groups = {dig_immediate=2,not_in_creative_inventory=1, mesecon_effector_on=1, mesecon_effector_off=1, mesecon=2},
 	walkable = false,
 	selection_box = {
 		type = "fixed",
@@ -167,79 +115,79 @@ minetest.register_node("mesecons_temperest:mesecon_socket_on", {
 		type = "fixed",
 		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
 	},
-	drop='"mesecons_temperest:mesecon_socket_off" 1',
+	drop = 'mesecons_temperest:mesecon_socket_off',
 	after_dig_node = function(pos)
 		mesecon:receptor_off(pos)
-	end
+	end,
+	mesecons = {receptor = {
+		state = mesecon.state.on
+	}},
 })
-
-mesecon:add_receptor_node("mesecons_temperest:mesecon_socket_on")
-mesecon:add_receptor_node_off("mesecons_temperest:mesecon_socket_off")
 
 minetest.register_craft({
 	output = '"mesecons_temperest:mesecon_socket_off" 2',
 	recipe = {
-		{'', '"default:steel_ingot"', ''},
-		{'"default:steel_ingot"', '"mesecons_temperest:mesecon_off"', '"default:steel_ingot"'},
-		{'', '"default:steel_ingot"', ''},
+		{'', 'default:steel_ingot', ''},
+		{'default:steel_ingot', 'mesecons_temperest:mesecon_off', 'default:steel_ingot'},
+		{'', 'default:steel_ingot', ''},
 	}
 })
 
 --TEMPEREST-INVERTER
-if true then
-	minetest.register_node("mesecons_temperest:mesecon_inverter_off", {
-		drawtype = "nodebox",
-		paramtype = "light",
-		is_ground_content = true,
-		tile_images = {"jeija_mesecon_inverter_off.png"},
-		groups = {dig_immediate=2,not_in_creative_inventory=1, mesecon_effector_on = 1, mesecon_effector_off = 1, mesecon = 2},
-		walkable = false,
-		selection_box = {
-			type = "fixed",
-		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
-		},
-		node_box = {
-			type = "fixed",
-		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
-		},
-		drop='"mesecons_temperest:mesecon_inverter_on" 1',
-	})
 
-	minetest.register_node("mesecons_temperest:mesecon_inverter_on", {
-		description = "Inverter",
-		drawtype = "nodebox",
-		paramtype = "light",
-		is_ground_content = true,
-		tile_images = {"jeija_mesecon_inverter_on.png"},
-		inventory_image = "jeija_mesecon_inverter_on.png",
-		wield_image = "jeija_mesecon_inverter_on.png",
-		groups = {dig_immediate=2, mesecon_effector_on = 1, mesecon_effector_off = 1, mesecon = 2},
-		walkable = false,
-		selection_box = {
-			type = "fixed",
-		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
-		},
-		node_box = {
-			type = "fixed",
-		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
-		},
-		after_place_node = function(pos)
-			mesecon:receptor_on(pos)
-		end,
-		after_dig_node = function(pos)
-			mesecon:receptor_off(pos)
-		end
-	})
+minetest.register_node("mesecons_temperest:mesecon_inverter_off", {
+	drawtype = "nodebox",
+	paramtype = "light",
+	tile_images = {"jeija_mesecon_inverter_off.png"},
+	groups = {dig_immediate=2,not_in_creative_inventory=1, mesecon_effector_on=1, mesecon_effector_off=1, mesecon=2},
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+	fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
+	},
+	node_box = {
+		type = "fixed",
+	fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
+	},
+	drop = 'mesecons_temperest:mesecon_inverter_on',
+	mesecons = {receptor = {
+		state = mesecon.state.off
+	}},
+})
 
-	mesecon:add_receptor_node("mesecons_temperest:mesecon_inverter_on")
-	mesecon:add_receptor_node_off("mesecons_temperest:mesecon_inverter_off")
+minetest.register_node("mesecons_temperest:mesecon_inverter_on", {
+	description = "Inverter",
+	drawtype = "nodebox",
+	paramtype = "light",
+	tile_images = {"jeija_mesecon_inverter_on.png"},
+	inventory_image = "jeija_mesecon_inverter_on.png",
+	wield_image = "jeija_mesecon_inverter_on.png",
+	groups = {dig_immediate=2, mesecon_effector_on=1, mesecon_effector_off=1, mesecon=2},
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+	fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
+	},
+	node_box = {
+		type = "fixed",
+		fixed = { -8/16, -8/16, -8/16, 8/16, -7/16, 8/16 },
+	},
+	after_place_node = function(pos)
+		mesecon:receptor_on(pos)
+	end,
+	after_dig_node = function(pos)
+		mesecon:receptor_off(pos)
+	end,
+	mesecons = {receptor = {
+		state = mesecon.state.on
+	}},
+})
 
-	minetest.register_craft({
-		output = '"mesecons_temperest:mesecon_inverter_on" 2',
-		recipe = {
-			{'"mesecons_temperest:mesecon_off"', '"default:steel_ingot"', '"group:mesecon_conductor_craftable"'},
-			{'"default:steel_ingot"', '', '"default:steel_ingot"'},
-			{'"group:mesecon_conductor_craftable"', '"default:steel_ingot"', '"group:mesecon_conductor_craftable"'},
-		}
-	})
-end
+minetest.register_craft({
+	output = 'mesecons_temperest:mesecon_inverter_on 2',
+	recipe = {
+		{'mesecons_temperest:mesecon_off', 'default:steel_ingot', 'group:mesecon_conductor_craftable'},
+		{'default:steel_ingot', '', 'default:steel_ingot'},
+		{'group:mesecon_conductor_craftable', 'default:steel_ingot', 'group:mesecon_conductor_craftable'},
+	}
+})
